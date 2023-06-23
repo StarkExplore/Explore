1aa9850f39df607740ed3ee7137a805cb0c29ee9
Willem Olding <willemolding@gmail.com>
2023-06-23 13:32:01 +1000

    adds accessors for game entity

--- ExploreTui/src/game_interface.rs
+++ ExploreTui/src/game_interface.rs
@@ -1,4 +1,6 @@
+use anyhow::Result;
 use starknet::{accounts::{SingleOwnerAccount}, providers::{JsonRpcClient, jsonrpc::HttpTransport}, signers::LocalWallet};
+use starknet::core::types::{BlockId, BlockTag, FieldElement};
 use dojo_world::world::WorldContract;
 
 type LocalAccount = SingleOwnerAccount<JsonRpcClient<HttpTransport>, LocalWallet>;
@@ -13,5 +15,16 @@ impl<'a> GameInterface<'a> {
         GameInterface {
             world
         }
+    }
+
+    // return the raw bytes of the entity for a given component given its name and entity key
+    pub async fn get_component_raw(&self, name: String, keys: Vec<FieldElement>) -> Result<Vec<FieldElement>> {
+        let component = self.world.component(&name, BlockId::Tag(BlockTag::Pending)).await?;
+        Ok(component.entity(0_u8.into(), keys, BlockId::Tag(BlockTag::Pending)).await?)
+    }
+
+    // gets the game for the current account
+    pub async fn get_game(&self) -> Result<Vec<FieldElement>> {
+        self.get_component_raw(String::from("Game"), vec![self.world.address.into()]).await
     }
 }
