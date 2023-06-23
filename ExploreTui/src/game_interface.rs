b7b9167b1242c8d63b8145adf1a2bfecacc5bfe2
Willem Olding <willemolding@gmail.com>
2023-06-23 13:46:11 +1000

    add auto detecting of environment

--- ExploreTui/src/game_interface.rs
+++ ExploreTui/src/game_interface.rs
@@ -18,13 +18,24 @@ impl<'a> GameInterface<'a> {
     }
 
     // return the raw bytes of the entity for a given component given its name and entity key
-    pub async fn get_component_raw(&self, name: String, keys: Vec<FieldElement>) -> Result<Vec<FieldElement>> {
-        let component = self.world.component(&name, BlockId::Tag(BlockTag::Pending)).await?;
+    async fn get_component_raw(&self, name: &str, keys: Vec<FieldElement>) -> Result<Vec<FieldElement>> {
+        let component = self.world.component(name, BlockId::Tag(BlockTag::Pending)).await?;
         Ok(component.entity(0_u8.into(), keys, BlockId::Tag(BlockTag::Pending)).await?)
+    }
+
+    async fn execute_system_raw(&self, system: &str, calldata: Vec<FieldElement>) -> Result<FieldElement> {
+        let res =
+        self.world.execute(&system, calldata).await?;
+        Ok(res.transaction_hash)
     }
 
     // gets the game for the current account
     pub async fn get_game(&self) -> Result<Vec<FieldElement>> {
-        self.get_component_raw(String::from("Game"), vec![self.world.address.into()]).await
+        self.get_component_raw("Game", vec![self.world.address.into()]).await
     }
+
+    pub async fn create_game(&self, name: FieldElement) -> Result<FieldElement> {
+        self.execute_system_raw("Move", vec![name]).await
+    }
+    
 }
--- ExploreTui/src/main.rs
+++ ExploreTui/src/main.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
-use std::{error::Error, path::PathBuf};
+use options::dojo_metadata_from_workspace;
+use scarb::core::Config;
+use std::{error::Error, path::PathBuf, env};
 use crate::options::{world::WorldOptions, starknet::StarknetOptions, account::AccountOptions};
 
 use dojo_world::world::WorldContract;
@@ -17,7 +19,7 @@ pub struct Args {
     #[arg(hide_short_help = true)]
     #[arg(env = "DOJO_MANIFEST_PATH")]
     #[arg(help = "Override path to a directory containing a Scarb.toml file.")]
-    pub manifest_path: Option<PathBuf>,
+    pub manifest_path: Option<camino::Utf8PathBuf>,
 
     #[command(flatten)]
     pub world: WorldOptions,
@@ -32,9 +34,25 @@ pub struct Args {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
-    let env_metadata = None;
     
     let Args { world, starknet, account, .. } = args;
+
+    let manifest_path = scarb::ops::find_manifest_path(args.manifest_path.as_deref())?;
+
+    let config = Config::builder(manifest_path)
+        .build()?;
+
+    let env_metadata = if config.manifest_path().exists() {
+        let ws = scarb::ops::read_workspace(config.manifest_path(), &config)?;
+        let env_metadata = dojo_metadata_from_workspace(&ws)
+            .and_then(|dojo_metadata| dojo_metadata.get("env").cloned());
+        env_metadata
+            .as_ref()
+            .and_then(|env_metadata| env_metadata.get(ws.config().profile().as_str()).cloned())
+            .or(env_metadata)
+    } else {
+        None
+    };
 
     let world_address = world.address(env_metadata.as_ref())?;
     let provider = starknet.provider(env_metadata.as_ref())?;
