ed6a1d0eb0dddada410af1970b7a780709daaf68
Willem Olding <willemolding@gmail.com>
2023-06-24 13:14:53 +1000

    refactor main and args mod

--- ExploreTui/src/main.rs
+++ ExploreTui/src/main.rs
@@ -1,11 +1,13 @@
-use crate::options::{account::AccountOptions, starknet::StarknetOptions, world::WorldOptions};
+
 use clap::Parser;
-use options::dojo_metadata_from_workspace;
-use scarb::core::Config;
 
-use dojo_world::world::WorldContract;
-use minesweeper::MinesweeperInterface;
 
+
+
+
+use args::Args;
+
+mod args;
 mod components;
 mod display;
 mod minesweeper;
@@ -13,63 +15,11 @@ mod movement;
 mod options;
 mod rpc_game_interface;
 
-/// Terminal interface for StarkExplore
-#[derive(Parser, Debug)]
-#[command(version, about)]
-pub struct Args {
-    #[arg(long)]
-    #[arg(global = true)]
-    #[arg(hide_short_help = true)]
-    #[arg(env = "DOJO_MANIFEST_PATH")]
-    #[arg(help = "Override path to a directory containing a Scarb.toml file.")]
-    pub manifest_path: Option<camino::Utf8PathBuf>,
-
-    #[command(flatten)]
-    pub world: WorldOptions,
-
-    #[command(flatten)]
-    pub starknet: StarknetOptions,
-
-    #[command(flatten)]
-    pub account: AccountOptions,
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
-
-    let Args {
-        world,
-        starknet,
-        account,
-        ..
-    } = args;
-
-    let manifest_path = scarb::ops::find_manifest_path(args.manifest_path.as_deref())?;
-
-    let config = Config::builder(manifest_path).build()?;
-
-    let env_metadata = if config.manifest_path().exists() {
-        let ws = scarb::ops::read_workspace(config.manifest_path(), &config)?;
-        let env_metadata = dojo_metadata_from_workspace(&ws)
-            .and_then(|dojo_metadata| dojo_metadata.get("env").cloned());
-        env_metadata
-            .as_ref()
-            .and_then(|env_metadata| env_metadata.get(ws.config().profile().as_str()).cloned())
-            .or(env_metadata)
-    } else {
-        None
-    };
-
-    let world_address = world.address(env_metadata.as_ref())?;
-    let provider = starknet.provider(env_metadata.as_ref())?;
-
-    let account = account.account(provider, env_metadata.as_ref()).await?;
-    let world = WorldContract::new(world_address, &account);
-
-    let interface = rpc_game_interface::RpcGameInterface::new(world);
-
-    let _game = interface.get_game().await?;
+    let account = args.get_account().await?;
+    let interface = args.build_starknet_interface(&account)?;
 
     display::start(interface).await?;
 
